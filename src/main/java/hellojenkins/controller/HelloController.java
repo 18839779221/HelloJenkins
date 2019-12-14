7b6f2dd506394339c477033c2debe643c4b2ee9a
skka <2569877795@qq.com>
2019-12-14 12:58:49 +0800

    add main

---
+++ src/main/java/hellojenkins/controller/HelloController.java
@@ -0,0 +1,13 @@
+package hellojenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Congratulation,you have succeed to deploy the project!";
+    }
+}
